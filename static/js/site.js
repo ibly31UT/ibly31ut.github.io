fb7db402083526e7b02ac58c44a1cd82a39e5015
ibly31UT <billy_connolly@utexas.edu>
2016-08-02 00:37:33 -0500

    Updated design
    Trying to get authentication access token.

--- static/js/site.js
+++ static/js/site.js
@@ -5,6 +5,18 @@ function Site() {
 	var authUrl = null;
 	var randKey = null;
 
+	this.Rest = {
+		AccessToken: "/api/v1/access_token",
+		Me: "/api/v1/me"
+	};
+
+	this.Config = {
+		consumerKey: "1U3eY8uqCUIaBA",
+		consumerSecret: "WLM29BUoWE7RS8WSj912hlywwcI",
+		redirectUri: "https://ibly31ut.github.io/index.html",
+		authorizationCode: "invalid",
+	};
+
 	this.Elements = null;
 	this.LoadElements = LoadElements;
 	this.AuthSuccess = AuthSuccess;
@@ -27,15 +39,17 @@ function Site() {
 
 	function GotInfo(result) {
 		window.console.log(result);
-		window.console.log("User: " + result.name);
-		this.Elements.AuthConfirmation.text(result.name).fadeIn().done(function() {
+
+		this.Elements.AuthConfirmation.text(result.scope).fadeIn().done(function() {
 			this.Elements.AuthButton.fadeOut();
 		});
 	}
 
 	function SetupOAuth() {
-		var match = ('' + window.location.hash).match(/access_token=(.*?)&/);
-		var accessToken = match ? match[1] : '';
+		var match = ('' + window.location.href).match(/state=(.*?)&code=(.*)&?/);
+		window.console.log(match);
+		var state = match ? match[1] : '';
+		var code = match ? match[2] : '';
 
 		randKey = Math.random().toString(36).substring(2);
 		reddit = new Snoocore({
@@ -43,33 +57,40 @@ function Site() {
 			oauth: { 
 				type: "explicit", // required when using implicit OAuth
 				mobile: false, // defaults to false.
-				consumerKey: "1U3eY8uqCUIaBA", 
-				consumerSecret: "WLM29BUoWE7RS8WSj912hlywwcI",
+				consumerKey: this.Config.consumerKey, 
+				consumerSecret: this.Config.consumerSecret,
 				state: randKey,
-				redirectUri: "https://ibly31ut.github.io/index.html",
+				redirectUri: this.Config.redirectUri,
 				scope: [ "flair", "identity", "history", "edit", "read", "subscribe", "vote" ]
 			}
 		});
 
-		window.console.log("Access token: ");
-		window.console.log(accessToken);
-		window.console.log(window.location.hash);
+		window.console.log(window.location.href);
 
-		if(accessToken){
-			reddit.auth(accessToken).then(function () {
-                this.AuthSuccess();
-                return reddit('/api/v1/me').get();
-            }).done(function (result) {
-            	this.GotInfo(result);
-            });
+		if(code){
+			this.Config.authorizationCode = code;
+			window.console.log("state: " + state);
+			window.console.log("code: " + code);
+
+			var result = reddit.auth(code).always(function(result){
+				window.console.log("Done result: ");
+				window.console.log(result);
+			});
+			window.console.log("Immediately after:");
+			window.console.log(result);
+
+			// .done(function (result) {
+   //          	this.GotInfo(result);
+   //          });
 		}
 
 		authUrl = reddit.getExplicitAuthUrl();
+		open()
 	}
 
 	function BindEvents() {
 		this.Elements = LoadElements();
-		this.Elements.AuthButton.on("click", function (evt){
+		this.Elements.AuthButton.on("click", function (){
 			window.location = authUrl;
 		});
 	}
